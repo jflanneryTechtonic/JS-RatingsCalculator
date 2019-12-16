f81c5e308bba826c9c22b4d7b38897dfa275305d
James Flannery <51173206+jflanneryTechtonic@users.noreply.github.com>
2019-12-16 10:31:39 -0700

    Final

--- js/ratings.js
+++ js/ratings.js
@@ -9,5 +9,15 @@ function collect_ratings(){
   elements.forEach(element => {
     rating = parseInt(element.id.replace('star', ''))
     ratings.count += parseInt(element.value)
+    ratings.sum += parseInt(element.value) * rating
+    if(ratings.count != 0){
+      ratings.average = ratings.sum / ratings.count
+    }
   });
+  return ratings
 }
+
+document.addEventListener('change', () => {
+  const ratings = collect_ratings();
+  document.querySelector('#average').value = ratings.average.toFixed(2);
+})
